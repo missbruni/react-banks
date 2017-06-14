d9e9e4062374ebba038a6d6f2370cd5ed01bd9ba
Bruna Lima <bruna.lima@veinteractive.com>
2017-06-14 11:34:54 +0100

    add message component

---
+++ src/components/Message.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Message.css';
+import logo from '../images/logo.png'
+
+class Message extends Component {
+  render() {
+    return (
+      <div className='message-component'>
+        <img className='message-logo' alt='logo' src={logo} />
+        <p className='message-header'>{this.props.message1}</p>
+        <p className='message-footer'>{this.props.message2}</p>
+      </div>
+    )
+  }
+}
+
+export default Message;
